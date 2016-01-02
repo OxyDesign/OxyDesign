4979751b8d6e7466565edc8e02a4563b3a91c8c8
Nicolas Escoffier <nico.oxydesign@free.fr>
2016-01-02 22:37:17 +0000

    Structure + Tweets + Logo (SVG) + Base Style

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
-console.log(twitterFetcher);
+window.onload = function(){
+  twitterFetcher.fetch({
+    id:'659879377698729984',
+    dataOnly: true,
+    maxTweets: 5,
+    customCallback: function(data){
+      var html = ''
+      data.forEach(function(tweet){
+        html += '<li>'
+          + (tweet.image ? '<img src="'+tweet.image+'" />' : '')
+          + '<p class="tweet-content">' + tweet.tweet + '</p>'
+          + '<p class="tweet-infos">' + tweet.time + '</p>'
+        + '</li>';
+      });
+      document.getElementById('tweets').innerHTML = html;
+    }
+  });
+};
