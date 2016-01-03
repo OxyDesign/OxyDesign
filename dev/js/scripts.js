ad7faeb9db92c4fd63da5c705be1d63859c4b6ba
Nicolas Escoffier <nico.oxydesign@free.fr>
2016-01-03 14:47:44 +0000

    Twitter Icon + H1 Style + Global Object : Toggle

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,31 @@
 window.onload = function(){
+  var toggles = Array.prototype.slice.apply(document.querySelectorAll('.toggle'));
+  toggles.forEach(function(toggle){
+    toggle.classList.add('closed');
+  });
+
+  document.querySelector('.global-object').addEventListener('click',function(e){
+    var looping = true,
+        currentElt = e.target;
+
+    for(; looping; ){
+      if(currentElt.classList.contains('toggle')){
+        currentElt.classList.toggle('closed');
+        looping = false;
+      } else if(currentElt.nodeName === 'A' ||  currentElt === this){
+        looping = false;
+      } else currentElt = currentElt.parentElement;
+    }
+  });
+
+  document.querySelector('.bt-toggle').addEventListener('click',function(e){
+    var toggleMehtod = this.classList.contains('closed') ? 'remove' : 'add';
+    this.classList[toggleMehtod]('closed');
+    toggles.forEach(function(toggle){
+      toggle.classList[toggleMehtod]('closed');
+    });
+  });
+
   twitterFetcher.fetch({
     id:'659879377698729984',
     dataOnly: true,
