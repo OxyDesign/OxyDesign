26b32a23b06e4370072703f90121f28d3757e266
Nicolas Escoffier <nico.oxydesign@free.fr>
2016-01-12 23:58:02 +0000

    Skills with Charts (JS)

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,70 @@
 window.onload = function(){
-  var toggles = Array.prototype.slice.apply(document.querySelectorAll('.toggle')),
+  var charts = Array.prototype.slice.call(document.querySelectorAll('.dynamic-chart')),
+      toggles = Array.prototype.slice.apply(document.querySelectorAll('.toggle')),
       wrapper = document.querySelector('.wrapper');
+
+  charts.forEach(function(chart){
+    var container = chart.querySelector('.chart'),
+        fragment = document.createDocumentFragment('div'),
+        items = Array.prototype.slice.call(chart.querySelectorAll('li')).map(function(item){
+          var value = item.querySelector('span');
+          value.style.display = 'none';
+          return parseInt(value.textContent,10);
+        }),
+        total = items.reduce(function(prev,cur){return prev+cur},0),
+        progression = 0;
+
+    container.classList.add('inactive');
+
+    items = items.map(function(cur,idx,arr){
+      return {
+        circle : document.createElement('div'),
+        angle : progression = (cur * 360 / total) + progression
+      };
+    });
+
+    items.reduceRight(function(prev,cur){
+      var angle = cur.angle;
+      var circle = cur.circle;
+      var newLeftAngle;
+
+      circle.classList.add('circle');
+
+      var halfRight = circle.appendChild(document.createElement('div'));
+      var halfCircleRight = halfRight.appendChild(document.createElement('div'));
+
+      halfRight.classList.add('half-right');
+      halfCircleRight.classList.add('half-circle');
+
+      if (angle > 180){
+        newLeftAngle = 180;
+
+        var halfLeft = circle.insertBefore(document.createElement('div'),halfRight);
+        var halfCircleLeft = halfLeft.appendChild(document.createElement('div'));
+
+        halfLeft.classList.add('half-left');
+        halfCircleLeft.classList.add('half-circle');
+
+        halfCircleLeft.style.webkitTransform = halfCircleLeft.style.MozTransform = halfCircleLeft.style.msTransform = halfCircleLeft.style.transform = 'rotate('+(angle-180+45)+'deg)';
+      }
+
+      halfCircleRight.style.webkitTransform = halfCircleRight.style.MozTransform = halfCircleRight.style.msTransform = halfCircleRight.style.transform = 'rotate('+((newLeftAngle || angle)+45)+'deg)';
+
+      fragment.appendChild(circle);
+
+      return angle;
+    },0);
+
+    container.appendChild(fragment);
+
+    setTimeout(function(){
+      container.classList.remove('inactive');
+    },500);
+
+    container.addEventListener('click',function(){
+      this.classList.toggle('inactive');
+    });
+  });
 
   toggles.forEach(function(toggle){
     toggle.classList.add('closed');
